10ab02679eb4eeb4836843ded6e68460f7b79f5f
fishbrightwise <147294128+fishbrightwise@users.noreply.github.com>
2023-11-05 00:34:04 +0800

    Updated joinevent.js
    Now, the user can join events and it will update to prevent the user from joining again.

--- joinevent.js
+++ joinevent.js
@@ -14,20 +14,73 @@ async function loadEvents() {
         if (i !== 'dummy') {
             // If it is your own created session. It will tell you.
             if (response.data[i].host == session) {
-                output += `<label><b>Your Session</b></label>`
+                output += `<label><b>Your Session</b></label>
+                        <div>
+                        <p>` + response.data[i].name + `</p>
+                        <p>` + response.data[i].description + `</p>
+                        <p>` + response.data[i].location + `</p>
+                        <p>From: ` + response.data[i].from + `</p>
+                        <p>To: ` + response.data[i].to + `</p>
+                        <p>Sign Up By: ` + response.data[i].signup + `</p>
+                    </div>
+                    </br><br>
+                `;
             }
-            output += `
-                <div>
-                    <p>` + response.data[i].name + `</p>
-                    <p>` + response.data[i].description + `</p>
-                    <p>` + response.data[i].location + `</p>
-                    <p>From: ` + response.data[i].from + `</p>
-                    <p>To: ` + response.data[i].to + `</p>
-                    <p>Sign Up By: ` + response.data[i].signup + `</p>
-                </div>
-                </br>
-            `;
+            else {
+                let participants = response.data[i].participants;
+                let check = true;
+                for (n in participants) {
+                    if (n == session) {
+                        check = false;
+                    }
+                }
+                if (check === true) {
+                    output += `
+                        <div>
+                            <p>` + response.data[i].name + `</p>
+                            <p>` + response.data[i].description + `</p>
+                            <p>` + response.data[i].location + `</p>
+                            <p>From: ` + response.data[i].from + `</p>
+                            <p>To: ` + response.data[i].to + `</p>
+                            <p>Sign Up By: ` + response.data[i].signup + `</p>
+                            <button onclick='joinEvent(` + JSON.stringify(i) + `)'>Register</button
+                        </div>
+                        </br><br>
+                    `;
+                }
+                else {
+                    output += `
+                        <div>
+                            <p>` + response.data[i].name + `</p>
+                            <p>` + response.data[i].description + `</p>
+                            <p>` + response.data[i].location + `</p>
+                            <p>From: ` + response.data[i].from + `</p>
+                            <p>To: ` + response.data[i].to + `</p>
+                            <p>Sign Up By: ` + response.data[i].signup + `</p>
+                            <p><i>You have already joined this event</i></p>
+                        </div>
+                        </br><br>
+                `;
+                }
+            }
         }
     }
     document.getElementById("events").innerHTML = output;
 };
+
+async function joinEvent(eventId) {
+    const url = dbUrlpt1 + '/events/' + eventId + dbUrlpt2;
+    const response = await axios.get(url);
+    
+    let participants = response.data.participants;
+    participants[session] = session;
+
+    let data = {
+        participants: participants
+    };
+
+    axios.patch(url, data)
+    .then(() => {
+        window.location.reload();
+    })
+}
